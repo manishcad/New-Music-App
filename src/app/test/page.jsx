edff7e24679870a7e65ec1bcb9a59ac1fa3e5a57
Manish Chand <manishtochand@gmail.com>
2025-04-04 16:58:36 +0530

    Add files via upload

---
+++ src/app/test/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import NewPlayer from "../components/MusicPlayer/NewPlayer";
+
+export default function Test() {
+  return (
+    <div>
+      
+      <NewPlayer />
+    </div>
+  );
+}
